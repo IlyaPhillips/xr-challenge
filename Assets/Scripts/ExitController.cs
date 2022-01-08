3087dea2669b0f89bcfb5a838a0ad05f4354f3f8
IlyaPhillips <68126787+IlyaPhillips@users.noreply.github.com>
2022-01-08 18:15:04 +0000

    Made Exit Prefab, added star map to second level

--- Assets/Scripts/ExitController.cs
+++ Assets/Scripts/ExitController.cs
@@ -22,6 +22,8 @@ public class ExitController : MonoBehaviour
             _stars.Add(pickups.transform.GetChild(i));
             var starPos = new Vector3(_stars[i].position.x / 5, (_stars[i].position.z+5) / 5 , 4.99f);
             Instantiate(starPrefab,starPos,Quaternion.Euler(0,90,0),starMap);
+            //TODO create list to store Prefabs and use list to draw prefabs, if collected remove from list
+            //TODO make door open when list is empty
         }
         
     }
