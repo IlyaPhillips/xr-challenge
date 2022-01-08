10a9fd898cb4bd45b722f081562aa464364b22a1
IlyaPhillips <68126787+IlyaPhillips@users.noreply.github.com>
2022-01-08 18:04:59 +0000

    Added Star Map to Door

--- Assets/Scripts/ExitController.cs
+++ Assets/Scripts/ExitController.cs
@@ -8,15 +8,21 @@ public class ExitController : MonoBehaviour
     [Header("References")]
     [SerializeField] private GameObject door;
 
-    private void OnEnable()
-    {
-        //Pickup.OnPickUp += PickedUp;
-    }
+    [SerializeField] private GameObject pickups;
 
+    [SerializeField] private GameObject starPrefab;
+    [SerializeField] private Transform starMap;
     
-
+    private List<Transform> _stars;
     void Start()
     {
+        _stars = new List<Transform>();
+        for (int i = 0; i < pickups.transform.childCount; i++)
+        {
+            _stars.Add(pickups.transform.GetChild(i));
+            var starPos = new Vector3(_stars[i].position.x / 5, (_stars[i].position.z+5) / 5 , 4.99f);
+            Instantiate(starPrefab,starPos,Quaternion.Euler(0,90,0),starMap);
+        }
         
     }
 
